5f065b409634472d5030e1dae39f2f1ea79e5ff3
Marcos Vinícius Queiroz <49909597+Kieroth29@users.noreply.github.com>
2021-02-26 01:34:49 -0300

    Seletor para múltiplas páginas

--- front/seletor_imagens.js
+++ front/seletor_imagens.js
@@ -1,45 +1,91 @@
 $(function () {
 	
 	var cnt = 1;
-	
-	$('#btn-d').click(function(){
-		if(cnt == 3){
-			cnt = 1;
-		}else{
-			cnt++;
-		}
-		switch(cnt){
-		case 1:
-			$('#dsp-img').attr('src','imgs/bs1.jpg');
-			break;
-		case 2:
-			$('#dsp-img').attr('src','imgs/bs2.jpg');
-			break;
-		case 3:
-			$('#dsp-img').attr('src','imgs/bs3.jpg');
-			break;
-	}
-	});
+	var url = window.location.pathname;
+	alert(url);
+	switch(url){
+		case "/C:/Users/Pichau/Desktop/Gift%20Virtual%20School/front/baiadosancho.html":
+			$('#btn-d').click(function(){
+				if(cnt == 3){
+					cnt = 1;
+				}else{
+					cnt++;
+				}
+				switch(cnt){
+				case 1:
+					$('#dsp-img').attr('src','imgs/bs1.jpg');
+					break;
+				case 2:
+					$('#dsp-img').attr('src','imgs/bs2.jpg');
+					break;
+				case 3:
+					$('#dsp-img').attr('src','imgs/bs3.jpg');
+					break;
+			}
+			});
 
-	$('#btn-e').click(function(){
-		if(cnt == 1){
-			cnt = 3;
-		}else{
-			cnt--;
-		}
-		switch(cnt){
-		case 1:
-			$('#dsp-img').attr('src','imgs/bs1.jpg');
+			$('#btn-e').click(function(){
+				if(cnt == 1){
+					cnt = 3;
+				}else{
+					cnt--;
+				}
+				switch(cnt){
+				case 1:
+					$('#dsp-img').attr('src','imgs/bs1.jpg');
+					break;
+				case 2:
+					$('#dsp-img').attr('src','imgs/bs2.jpg');
+					break;
+				case 3:
+					$('#dsp-img').attr('src','imgs/bs3.jpg');
+					break;
+			}
+			});
 			break;
-		case 2:
-			$('#dsp-img').attr('src','imgs/bs2.jpg');
-			break;
-		case 3:
-			$('#dsp-img').attr('src','imgs/bs3.jpg');
-			break;
-	}
-	});
 
+		case "/C:/Users/Pichau/Desktop/Gift%20Virtual%20School/front/carneiros.html":
+			$('#btn-d').click(function(){
+				if(cnt == 3){
+					cnt = 1;
+				}else{
+					cnt++;
+				}
+				switch(cnt){
+				case 1:
+					$('#dsp-img').attr('src','imgs/pc1.jpg');
+					break;
+				case 2:
+					$('#dsp-img').attr('src','imgs/pc2.jpg');
+					break;
+				case 3:
+					$('#dsp-img').attr('src','imgs/pc3.jpg');
+					break;
+			}
+			});
+
+			$('#btn-e').click(function(){
+				if(cnt == 1){
+					cnt = 3;
+				}else{
+					cnt--;
+				}
+				switch(cnt){
+				case 1:
+					$('#dsp-img').attr('src','imgs/pc1.jpg');
+					break;
+				case 2:
+					$('#dsp-img').attr('src','imgs/pc2.jpg');
+					break;
+				case 3:
+					$('#dsp-img').attr('src','imgs/pc3.jpg');
+					break;
+			}
+			});
+			break;
+
+
+	}
 	
 
 })
